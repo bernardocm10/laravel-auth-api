dc4ef40efa932c2379e2618c6d74f3aac5bc252a
Bernardo Carneiro <bernardocm1089@gmail.com>
2026-05-03 14:47:00 +0000

    fix: corrige handlers de exception pra rotas da api

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,14 @@
 <?php
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,8 +18,43 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->redirectGuestsTo(function (Request $request) {
+            if ($request->is('api/*')) {
+                return null;
+            }
+        });
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (TokenExpiredException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json(['message' => 'Token expirado.'], 401);
+            }
+        });
+
+        $exceptions->render(function (TokenInvalidException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json(['message' => 'Token inválido.'], 401);
+            }
+        });
+
+        $exceptions->render(function (JWTException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json(['message' => 'Token ausente ou malformado.'], 401);
+            }
+        });
+
+        $exceptions->render(function (AuthenticationException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json(['message' => 'Não autenticado.'], 401);
+            }
+        });
+
+        $exceptions->render(function (ValidationException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => 'Dados inválidos.',
+                    'errors'  => $e->errors(),
+                ], 422);
+            }
+        });
     })->create();
