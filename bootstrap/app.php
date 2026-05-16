a19f51b28add1cea3e88347b0b43f488c501d0c2
Bernardo Carneiro <bernardocm1089@gmail.com>
2026-05-16 09:41:00 +0000

    feat: adicionar middleware global com headers HTTP de segurança

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->append(\App\Http\Middleware\SecurityHeaders::class);
+
         $middleware->redirectGuestsTo(function (Request $request) {
             if ($request->is('api/*')) {
                 return null;
