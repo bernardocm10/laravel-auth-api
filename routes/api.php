7773ebcd675f1064a56c3e3994adf7a40bcbabbb
Bernardo Carneiro <bernardocm1089@gmail.com>
2026-05-22 10:05:00 +0000

    feat: registrar rotas de senha e perfil na api

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,19 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\EmailVerificationController;
+use App\Http\Controllers\PasswordController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->group(function () {
     Route::middleware('throttle:10,1')->group(function () {
         Route::post('register', [AuthController::class, 'register']);
         Route::post('login', [AuthController::class, 'login']);
+    });
+
+    Route::middleware('throttle:5,1')->group(function () {
+        Route::post('forgot-password', [PasswordController::class, 'forgot']);
+        Route::post('reset-password', [PasswordController::class, 'reset']);
     });
 
     Route::get('email/verify/{id}/{hash}', [EmailVerificationController::class, 'verify'])
@@ -18,8 +25,10 @@ Route::prefix('auth')->group(function () {
         Route::middleware('throttle:60,1')->group(function () {
             Route::post('logout', [AuthController::class, 'logout']);
             Route::get('me', [AuthController::class, 'me']);
+            Route::put('profile', [ProfileController::class, 'update']);
         });
 
+        Route::put('password', [PasswordController::class, 'update'])->middleware('throttle:10,1');
         Route::post('refresh', [AuthController::class, 'refresh'])->middleware('throttle:10,1');
         Route::post('email/resend', [EmailVerificationController::class, 'resend'])->middleware('throttle:6,1');
     });
